91ff8bcad51691123a1839a44260c84d3ed67b2c
一ノ瀬由芽 <srv123123@me.com>
2018-10-26 14:07:13 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'Hello, Heroku';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>herikuテスト</title>
+</head>
+<body>
+    <h1>初めてのリリース</h1>
+    
+</body>
+</html>
